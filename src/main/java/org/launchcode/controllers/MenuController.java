6f22e4e55bfb320f8389b03b44d33cf8068e9efa
Miguel Cortes <miguel_cortes1@yahoo.com>
2019-09-11 18:50:44 -0500

    Yaaayyyy!!!

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -10,17 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "menu")
 public class MenuController {
-
 
 
     @Autowired
@@ -38,12 +34,13 @@ public class MenuController {
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
-        model.addAttribute("title","Add menu");
+        model.addAttribute("title", "Add menu");
         model.addAttribute(new Menu());
         return "menu/add";
     }
+
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String ass(Model model, @ModelAttribute @Valid Menu newMenu, Errors errors) {
+    public String adds(Model model, @ModelAttribute @Valid Menu newMenu, Errors errors) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Menu");
@@ -86,5 +83,22 @@ public class MenuController {
         menu.addItem(cheese);
         menuDao.save(menu);
         return "redirect:../view/" + menu.getId();
+    }
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveMenuForm(Model model) {
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "Remove Menu");
+        return "menu/remove";
+
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveMenuForm(@RequestParam int[] menuIds) {
+
+        for (int menuId : menuIds) {
+            menuDao.delete(menuId);
+        }
+
+        return "redirect:";
     }
 }
